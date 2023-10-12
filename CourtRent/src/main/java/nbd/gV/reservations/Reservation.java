52ee6f4ff67b22419e4764ef56fd49333b3b73d8
Mikorollo2608 <michal.p2608@wp.pl>
2023-10-12 22:05:12 +0200

    Changed int to UUID type for id in Reservation class

--- CourtRent/src/main/java/nbd/gV/reservations/Reservation.java
+++ CourtRent/src/main/java/nbd/gV/reservations/Reservation.java
@@ -9,17 +9,18 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.UUID;
 
 public class Reservation {
-    private final int id;
+    private final UUID id;
     private final Client client;
     private final Court court;
     private final LocalDateTime beginTime;
     private LocalDateTime endTime = null;
     private double reservationCost;
 
-    public Reservation(int id, Client client, Court court, LocalDateTime beginTime) {
-        if (id <= 0 || client == null || court == null)
+    public Reservation(UUID id, Client client, Court court, LocalDateTime beginTime) {
+        if (id == null || client == null || court == null)
             throw new MainException("Niepoprawny parametr przy tworzeniu obiektu rezerwacji!");
 
         this.id = id;
@@ -28,7 +29,7 @@ public class Reservation {
         this.beginTime = (beginTime == null) ? LocalDateTime.now() : beginTime;
     }
 
-    public int getId() {
+    public UUID getId() {
         return id;
     }
 
@@ -92,7 +93,7 @@ public class Reservation {
     }
 
     public String getReservationInfo() {
-        return "Rezerwacja nr %d przez '%s' boiska: '%s', od godziny [%s]%s%n".formatted(id,
+        return "Rezerwacja nr %s przez '%s' boiska: '%s', od godziny [%s]%s%n".formatted(id,
                 client.getClientInfo().replace("\n", ""),
                 court.getCourtInfo().replace("\n", ""),
                 beginTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)),
--- CourtRent/src/test/java/ReservationTest.java
+++ CourtRent/src/test/java/ReservationTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -24,22 +25,24 @@ public class ReservationTest {
     Client testClient;
     ClientType testClientType;
     Court testCourt;
+    UUID testUUID;
 
     @BeforeEach
     void setUp() {
         testClientType = new Normal();
         testClient = new Client("John", "Smith", "123456789", testClientType);
         testCourt = new FootballCourt(1, 100, 1);
+        testUUID = UUID.randomUUID();
     }
 
     @Test
     void testCreatingReservation() {
         LocalDateTime now = LocalDateTime.of(2023, Month.JUNE, 3, 22, 15);
         assertNotNull(now);
-        Reservation reservation = new Reservation(1, testClient, testCourt, now);
+        Reservation reservation = new Reservation(testUUID, testClient, testCourt, now);
         assertNotNull(reservation);
 
-        assertEquals(1, reservation.getId());
+        assertEquals(testUUID, reservation.getId());
         assertEquals(testClient, reservation.getClient());
         assertEquals(testCourt, reservation.getCourt());
         assertEquals(now, reservation.getBeginTime());
@@ -48,12 +51,12 @@ public class ReservationTest {
 
     @Test
     void testCreatingReservationWithNullDate() {
-        Reservation reservation = new Reservation(1, testClient, testCourt, null);
+        Reservation reservation = new Reservation(testUUID, testClient, testCourt, null);
         assertNotNull(reservation);
         LocalDateTime now = LocalDateTime.now();
         assertNotNull(now);
 
-        assertEquals(1, reservation.getId());
+        assertEquals(testUUID, reservation.getId());
         assertEquals(testClient, reservation.getClient());
         assertEquals(testCourt, reservation.getCourt());
         assertEquals(0, Duration.between(reservation.getBeginTime(), now).getSeconds());
@@ -63,16 +66,16 @@ public class ReservationTest {
 
     @Test
     void testConstructorException() {
-        assertThrows(MainException.class, () -> new Reservation(-5, testClient, testCourt, null));
-        assertThrows(MainException.class, () -> new Reservation(1, null, testCourt, null));
-        assertThrows(MainException.class, () -> new Reservation(1, testClient, null, null));
+        assertThrows(MainException.class, () -> new Reservation(null, testClient, testCourt, null));
+        assertThrows(MainException.class, () -> new Reservation(testUUID, null, testCourt, null));
+        assertThrows(MainException.class, () -> new Reservation(testUUID, testClient, null, null));
     }
 
     @Test
     void testEndingReservation() {
         LocalDateTime now = LocalDateTime.of(2023, Month.JUNE, 3, 22, 15);
         LocalDateTime then = LocalDateTime.of(2023, Month.JUNE, 3, 20, 7);
-        Reservation reservation = new Reservation(1, testClient, testCourt, then);
+        Reservation reservation = new Reservation(testUUID, testClient, testCourt, then);
         assertNotNull(reservation);
 
         assertEquals(0, reservation.getReservationHours());
@@ -99,7 +102,7 @@ public class ReservationTest {
     @Test
     void testEndingReservationWithNullDate() {
         LocalDateTime then = LocalDateTime.of(2023, Month.JUNE, 3, 20, 7);
-        Reservation reservation = new Reservation(1, testClient, testCourt, then);
+        Reservation reservation = new Reservation(testUUID, testClient, testCourt, then);
         assertNotNull(reservation);
 
         assertNull(reservation.getEndTime());
@@ -125,7 +128,7 @@ public class ReservationTest {
     void testExceedingReservationPermittedTime() {
         LocalDateTime now = LocalDateTime.of(2023, Month.JUNE, 3, 23, 15);
         LocalDateTime then = LocalDateTime.of(2023, Month.JUNE, 3, 19, 7);
-        Reservation reservation = new Reservation(1, testClient, testCourt, then);
+        Reservation reservation = new Reservation(testUUID, testClient, testCourt, then);
         assertNotNull(reservation);
 
         reservation.endReservation(now);
@@ -138,18 +141,20 @@ public class ReservationTest {
     void testGettingReservationInfo() {
         LocalDateTime now = LocalDateTime.of(2023, Month.JUNE, 3, 22, 15);
         LocalDateTime then = LocalDateTime.of(2023, Month.JUNE, 3, 20, 7);
-        Reservation reservation = new Reservation(1, testClient, testCourt, then);
+        Reservation reservation = new Reservation(testUUID, testClient, testCourt, then);
 
         assertNotNull(reservation);
 
-        String str = "Rezerwacja nr 1 przez 'Klient - John Smith o numerze PESEL 123456789' boiska: 'Boisko nr 1 " +
+        String str = "Rezerwacja nr " + testUUID +
+                " przez 'Klient - John Smith o numerze PESEL 123456789' boiska: 'Boisko nr 1 " +
                 "przeznaczone do pilki noznej o powierzchni 1,00 i koszcie za rezerwację: 150,00 PLN', " +
                 "od godziny [03.06.2023, 20:07].%n".formatted();
         assertEquals(str, reservation.getReservationInfo());
 
         reservation.endReservation(now);
 
-        str = ("Rezerwacja nr 1 przez 'Klient - John Smith o numerze PESEL 123456789' boiska: 'Boisko nr 1 " +
+        str = ("Rezerwacja nr " + testUUID +
+                " przez 'Klient - John Smith o numerze PESEL 123456789' boiska: 'Boisko nr 1 " +
                 "przeznaczone do pilki noznej o powierzchni 1,00 i koszcie za rezerwację: 150,00 PLN', " +
                 "od godziny [03.06.2023, 20:07] do godziny [03.06.2023, 22:15].%n").formatted();
         assertEquals(str, reservation.getReservationInfo());
