5fa20af81facec3e4fb6b8025cb2f4741d4c71a6
Mikorollo2608 <michal.p2608@wp.pl>
2023-11-05 21:57:38 +0100

    Changed ClientManager to using new MongoRepo and refactored tests for it

--- zad2/CourtRent/src/main/java/nbd/gV/clients/ClientManager.java
+++ zad2/CourtRent/src/main/java/nbd/gV/clients/ClientManager.java
@@ -1,74 +1,75 @@
-//package nbd.gV.clients;
-//
-//import jakarta.persistence.criteria.CriteriaBuilder;
-//import jakarta.persistence.criteria.CriteriaQuery;
-//import jakarta.persistence.criteria.Root;
-//import nbd.gV.exceptions.ClientException;
-//import nbd.gV.exceptions.CourtException;
-//import nbd.gV.exceptions.JakartaException;
-//import nbd.gV.exceptions.MainException;
-//import nbd.gV.repositories.ClientRepository;
-//
-//import java.util.List;
-//import java.util.UUID;
-//
-//public class ClientManager {
-//
-//    private final ClientRepository clientRepository;
-//
-//    public ClientManager(String unitName) {
-//        this.clientRepository = new ClientRepository(unitName);
-//    }
-//
-//    public ClientManager() {
-//        this.clientRepository = new ClientRepository("default");
-//    }
-//
-//    public Client registerClient(String firstName, String lastName, String personalID, ClientType clientType) {
-//        Client newClient = new Client(firstName, lastName, personalID, clientType);
-//        try {
-//            clientRepository.create(newClient);
-//        } catch (JakartaException exception) {
-//            throw new ClientException("Nie udalo sie zarejestrowac klienta w bazie!");
-//        }
-//        return newClient;
-//    }
-//
-//    public void unregisterClient(Client client) {
-//        if (client == null) {
-//            throw new MainException("Nie mozna wyrejestrowac nieistniejacego klienta!");
-//        }
-//        try {
-//            client.setArchive(true);
-//            clientRepository.update(client);
-//        } catch (JakartaException exception) {
-//            client.setArchive(false);
-//            throw new ClientException("Nie udalo sie wyrejestrowac podanego boiska.");
-//        }
-//    }
-//
-//    public Client getClient(UUID clientID) {
-//        try {
-//            return clientRepository.findByUUID(clientID);
-//        } catch (JakartaException exception) {
-//            throw new ClientException("Blad transakcji.");
-//        }
-//    }
-//
-//    public List<Client> getAllClients() {
-//        try {
-//            return clientRepository.findAll();
-//        } catch (JakartaException exception) {
-//            throw new CourtException("Nie udalo sie uzyskac clientow.");
-//        }
-//    }
-//
-//    public Client findCourtByPersonalId(String personalId) {
-//        CriteriaBuilder cb = clientRepository.getEntityManager().getCriteriaBuilder();
-//        CriteriaQuery<Client> query = cb.createQuery(Client.class);
-//        Root<Client> clientRoot = query.from(Client.class);
-//        query.select(clientRoot).where(cb.equal(clientRoot.get(Client_.PERSONAL_ID), personalId));
-//        List<Client> result = clientRepository.find(query);
-//        return result.isEmpty() ? null : result.get(0);
-//    }
-//}
+package nbd.gV.clients;
+
+import com.mongodb.client.model.Filters;
+import nbd.gV.exceptions.ClientException;
+import nbd.gV.exceptions.JakartaException;
+import nbd.gV.exceptions.MainException;
+import nbd.gV.exceptions.MyMongoException;
+import nbd.gV.mappers.ClientMapper;
+import nbd.gV.repositories.ClientMongoRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ClientManager {
+
+    private final ClientMongoRepository clientRepository;
+
+    public ClientManager() {
+        this.clientRepository = new ClientMongoRepository();
+    }
+
+    public Client registerClient(String firstName, String lastName, String personalID, ClientType clientType) {
+        Client newClient = new Client(firstName, lastName, personalID, clientType);
+        try {
+            if (!clientRepository.create(ClientMapper.toMongoClient(newClient))) {
+                throw new ClientException("Nie udalo sie zarejestrowac klienta w bazie! - brak odpowiedzi");
+            }
+        } catch (MyMongoException exception) {
+            throw new ClientException("Nie udalo sie zarejestrowac klienta w bazie!");
+        }
+        return newClient;
+    }
+
+    public void unregisterClient(Client client) {
+        if (client == null) {
+            throw new MainException("Nie mozna wyrejestrowac nieistniejacego klienta!");
+        }
+        try {
+            client.setArchive(true);
+            if (!clientRepository.update(client.getClientID(), "archive", true)) {
+                client.setArchive(false);
+                throw new ClientException("Nie udalo sie wyrejestrowac podanego klienta.");
+            }
+        } catch (Exception exception) {
+            client.setArchive(false);
+            throw new ClientException("Nie udalo sie wyrejestrowac podanego klienta. - nieznany blad");
+        }
+    }
+
+    public Client getClient(UUID clientID) {
+        try {
+            return ClientMapper.fromMongoClient(clientRepository.readByUUID(clientID));
+        } catch (JakartaException exception) {
+            throw new ClientException("Blad transakcji.");
+        }
+    }
+
+    public List<Client> getAllClients() {
+        try {
+            List<Client> clientsList = new ArrayList<>();
+            for (var el :  clientRepository.readAll()) {
+                clientsList.add(ClientMapper.fromMongoClient(el));
+            }
+            return clientsList;
+        } catch (Exception exception) {
+            throw new ClientException("Nie udalo sie uzyskac clientow.");
+        }
+    }
+
+    public Client findCourtByPersonalId(String personalId) {
+        return ClientMapper.fromMongoClient(
+                clientRepository.read(Filters.eq("personalid", personalId)).get(0));
+    }
+}
